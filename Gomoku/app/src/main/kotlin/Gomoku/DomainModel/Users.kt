c47e11a2fc6262dd568ed397a9773745350f90ff
49476 - Gonçalo Pinto <a49476@alunos.isel.pt>
2023-11-09 09:54:35 +0000

    Correct commit of project - falta alinhar Ranking

--- Gomoku/app/src/main/kotlin/Gomoku/DomainModel/Users.kt
+++
@@ -1,7 +0,0 @@
-package Gomoku.DomainModel
-
-import java.net.URL
-
-data class Users(val name: String, val password: String,val token : String? = null, val  url: URL )
-data class UsersFake(val name: String, val password: String)
-data class GamePlayer(val id: Int, val variantes: variantes, val openingRule: openingrule)
